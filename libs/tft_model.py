447a4f32f39dc59b9d46c97f0427d79c8bb249e3
vwitt <valeriewitt96@gmail.com>
2023-07-21 20:56:21 +0200

    ch

--- libs/tft_model.py
+++ libs/tft_model.py
@@ -1352,7 +1352,8 @@ class TemporalFusionTransformer(object):
     # Allows for direct serialisation of tensorflow variables to avoid spurious
     # issue with Keras that leads to different performance evaluation results
     # when model is reloaded (https://github.com/keras-team/keras/issues/4875).
-    tf.keras.models.save_model(model= self.model, filepath=model_folder)
+    #tf.keras.models.save_model(model= self.model, filepath=model_folder)
+    self.model.save(model_folder=model_folder)
     #utils.save(
      #   tf.compat.v1.keras.backend.get_session(),
       #  model_folder,
